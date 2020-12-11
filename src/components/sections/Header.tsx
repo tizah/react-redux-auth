e080280319a3c960916b5b21bc31828ffc2f22e5
tizah <davidzagi93@gmail.com>
2020-12-12 00:11:16 +0100

    added gallery upload

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory, Link, useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
 import { RootState } from "../../store";
@@ -8,6 +8,7 @@ import { signout } from "../../store/actions/authActions";
 
 const Header: FC = () => {
   const history = useHistory();
+  const location = useLocation();
   const dispatch = useDispatch();
   const { authenticated } = useSelector((state: RootState) => state.auth);
 
@@ -21,7 +22,13 @@ const Header: FC = () => {
         <div className="navbar-brand">
           <Link
             className="navbar-item"
-            to={!authenticated ? "/" : "/dashboard"}
+            to={
+              !authenticated
+                ? "/"
+                : location.pathname === "/dashboard"
+                ? "/"
+                : "/dashboard"
+            }
           >
             AppName
           </Link>
